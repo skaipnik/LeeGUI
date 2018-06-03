f55d8c13c9b1dcf5f6578edb6574a1f8945f7239
skaipnik <38259777+skaipnik@users.noreply.github.com>
2018-06-03 18:26:35 +0400

    add source file

---
+++ source/about.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'about.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import os
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setEnabled(True)
+        MainWindow.resize(690, 462)
+        MainWindow.setMinimumSize(QtCore.QSize(690, 462))
+        MainWindow.setMaximumSize(QtCore.QSize(690, 462))
+        MainWindow.setDocumentMode(False)
+        MainWindow.setUnifiedTitleAndToolBarOnMac(False)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(10, 430, 671, 31))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label.setScaledContents(False)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setEnabled(True)
+        self.textEdit.setGeometry(QtCore.QRect(10, 10, 671, 411))
+        self.textEdit.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.textEdit.setAcceptDrops(True)
+        self.textEdit.setObjectName("textEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        scriptDir = os.path.dirname(os.path.realpath(__file__))
+        self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'icon/icon.ico'))
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "О программе"))
+        self.label.setText(_translate("MainWindow", "Страшилин Никита, 2018"))
+        self.textEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Привет, cпасибо за использование моего програмного обеспечения!</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Вот небольшая инструукция что бы не запутаться:</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">(1) В строку длинна лабиринта введите целое положительное число, равное длинне желаемого лабиринта.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">(2) В строку стартовая позиция введите, через запятую, два целых положительных числа, не привышающих число длинны лабиринта.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Пример: </span><span style=\" font-size:10pt; text-decoration: underline;\">1,2</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt; text-decoration: underline;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">(3) В строку финишная позиция введите, через запятую, два целых положительных числа, не привышающих число длинны лабиринта.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Пример: </span><span style=\" font-size:10pt; text-decoration: underline;\">2,1</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt; font-weight:600;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">(4) В строку барьеры введите, через точку с запятой (;), несколько пар, состоящих из двух целых положительных числел, не привышающих число длинны лабиринта.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Пример: </span><span style=\" font-size:10pt; text-decoration: underline;\">2,3;3,4;4,5</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt; font-weight:600;\">!!!!!Важно, что бы все числа вводились строго через указанные знаки препинания и в строке НЕ присутствовали пробелы!!!!!</span></p>\n"
+
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Для того, что-бы использовать файл с исходными данными заполните его в той же последовательности и придеживаясь описанных ранее правил. В первую строчкку файла впишите длинну лабиринта, во вторую стартовую позицию и т.д. </span></p>\n"
+
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt; font-weight:600;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt; font-style:italic;\">Нашли ошбку или просто хотите поделиться своим мнением? Пишите на на электронную почту </span><a href=\"http://send-email.org/\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">skaip.nik@gmail.com</span></a><span style=\" font-size:10pt;\"> </span><span style=\" font-size:10pt; font-style:italic;\">c пометкой </span><span style=\" font-size:10pt;\"> </span><span style=\" font-size:10pt; text-decoration: underline;\">Lee.</span></p></body></html>"))
---
+++ source/main.py
@@ -0,0 +1,151 @@
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets
+import design  # Это наш конвертированный файл дизайна
+import about  # Это наш конвертированный файл окна About
+import Lee
+import numpy as np
+
+class MainWindow(QtWidgets.QMainWindow, about.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent=parent)
+        self.setupUi(self)
+
+class ExampleApp(design.Ui_Lee, MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.open_file.triggered.connect(self.open)
+        self.save_file.triggered.connect(self.save)
+        self.About.triggered.connect(self.about)
+        self.build.clicked.connect(self.build_maze)
+        self.b = None
+        self.p = None
+
+    def open(self):
+        try:
+            fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Открыть файл')[0]
+            s = open(fname).read()
+            array = [row.strip() for row in s]
+            arr = self.split_on(array)
+            leng = int(arr[0][0])
+            self.len.setText(str(leng))
+            start = self.separation([i for i in self.split_two(arr[1])])
+            self.start.setText(str(start))
+            finish = self.separation([i for i in self.split_two(arr[2])])
+            self.finish.setText(str(finish))
+            barriers = self.fuck([[b for b in i] for i in self.split_one(arr[3])])
+            self.barriers.setText(str(barriers))
+
+            field = Lee.Field(leng, tuple(start), tuple(finish), barriers)
+            field.emit()
+            self.b = np.array(field._show())
+            self.field.setText(str(self.b))
+        except FileNotFoundError:
+            print('No such file in directory')
+        except IndexError:
+            self.field.setText('В выбранном файле содержаться не корректные данные!')
+        else:
+            try:
+                path = field.get_path()
+                self.p = [p for p in path]
+                self.Way.setText(str(self.p))
+            except:
+                self.Way.setText('Не возможно проложить путь.')
+    
+    def save(self):
+        try:
+            fname = QtWidgets.QFileDialog.getSaveFileName(self, 'Сохранить файл', 'Result.txt')[0]
+            s = open(fname, 'w')
+            s.write('Maze:\n')
+            s.write(str(self.b))
+            s.write('\n\nWay:\n')
+            s.write(str(self.p))
+        except FileNotFoundError:
+            print('FileNotFoundError')
+
+    def about(self):
+        global window
+        window = MainWindow()
+        window.show()
+    
+    def split_on(self, what, delimiter = ''):
+        splitted = [[]]
+        for item in what:
+            if item == delimiter:
+                splitted.append([])
+            else:
+                splitted[-1].append(item)
+        
+        return splitted
+
+    def split_one(self, what, delimiter = ';'):
+        splitted = [[]]
+        for item in what:
+            if item == delimiter:
+                splitted.append([])
+            else:
+                splitted[-1].append(item)
+
+        return splitted
+
+    def split_two(self, what, delimiter = ','):
+        splitted = [[]]
+        for item in what:
+            if item == delimiter:
+                splitted.append([])
+            else:
+                splitted[-1].append(item)
+
+        return splitted
+
+    def fuck(self, tup):
+        split = []
+        for i in tup:
+            split.append(tuple(self.separation(self.split_two(i))))
+        return split
+
+    def separation(self, tup):
+        separat = []
+        for i in tup:
+            b = ''
+            for k in i:
+                kk = k + b
+                b = k
+            separat.append(int(kk[::-1]))
+        return separat
+
+    def build_maze(self):
+        try:
+            leng = int(self.len.text())
+            start = self.separation([i for i in self.split_two(self.start.text())])
+            finish = self.separation([i for i in self.split_two(self.finish.text())])
+            barriers = self.fuck([[b for b in i] for i in self.split_one(self.barriers.text())])
+            field = Lee.Field(leng, tuple(start), tuple(finish), barriers)
+            field.emit()
+            self.b = np.array(field._show())
+            self.field.setText(str(self.b))
+        except ValueError:
+            self.field.setText('Введите корректные данные!')
+        except UnboundLocalError:
+            self.field.setText('Видимо какие-то поля не были заполнены. Так нельзя, заполните все поля!')
+        else:
+            try:
+                path = field.get_path()
+                self.p = [p for p in path]
+                self.Way.setText(str(self.p))
+            except:
+                self.Way.setText('Не возможно проложить путь.')
+
+        
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    sys.exit(app.exec_())  # и запускаем приложение
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
